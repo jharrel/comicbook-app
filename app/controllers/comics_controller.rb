3f76f718689d6a5c1ddd56b7ae994a5a1c77d312
jharrel <harrel.jason@gmail.com>
2019-08-10 16:33:53 -0700

    added new pages and worked on page links between each other.

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -10,12 +10,39 @@ class ComicsController < ApplicationController
   end
 
   get '/comics/:id' do
-    @comic = Comic.find_by_id(params[:id])
+    set_comic
       erb :'comics/show'
   end
 
   post '/comics' do
-    comic = Comic.create(params)
+    comic = Comic.create(comic_params)
     redirect '/comics'
   end
+
+  get '/comics/:id/edit' do
+    set_comic
+    erb :'comics/edit'
+  end
+
+  patch '/comics/:id' do
+    set_comic
+    @comic.update(comic_params)
+    redirect "/comics/#{@comic.id}"
+  end
+
+  delete "/comics/:id" do
+    set_comic
+    @comic.destroy
+    redirect "/comics"
+  end
+
+  private
+
+    def set_comic
+      @comic = Comic.find_by_id(params[:id])
+    end
+
+    def comic_params
+      { title: params[:comic][:title], publisher: params[:comic][:publisher], issue: params[:comic][:issue] }
+    end
 end
